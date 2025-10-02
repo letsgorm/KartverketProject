bf16cdbec8a5076bc03bc199879ed54861085519
simesp26 <simonespe12@icloud.com>
2025-10-02 15:19:50 +0200

    Registrer og logg inn bruker, foreløpig uten hashing

---
+++ KartverketProject/Controllers/AuthenticationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KartverketProject.Models;
+using KartverketProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KartverketProject.Controllers
+
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController
+    {
+
+    }
+}
---
+++ KartverketProject/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace KartverketProject.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; } //NB: Will be hashed later 
+    }
+}
