d3a13549da5b803c1742b5ffae0ff041f259a732
simesp26 <simonespe12@icloud.com>
2025-10-22 15:48:25 +0200

    Register and login frontend, no backend connection

---
+++ KartverketProject/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KartverketProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(); // viser Views/Account/Login.cshtml
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(); // viser Views/Account/Register.cshtml
+        }
+    }
+}
+
